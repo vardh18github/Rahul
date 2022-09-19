6c723b2976df6a51a1aed1a03c9624ee7ee63d3a
vardh18github <vardhamanjain756@gmail.com>
2022-09-19 19:15:43 +0530

    New Linked List

---
+++ Main/src/com/velocity/LinkedList1.java
@@ -0,0 +1,32 @@
+
+
+
+
+
+package com.velocity;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedList1 {
+
+	public static void main(String[] args) {
+	LinkedList<Integer> df=new LinkedList<Integer>();
+	df.add(3);
+	df.add(34);
+	df.add(33);
+	df.add(3436);
+	
+	df.add(33);
+	
+	Iterator<Integer> it2 = df.iterator();
+	
+	while(it2.hasNext()) {
+		System.out.println(it2.next());
+	}
+	
+	
+	
+	}
+
+}
